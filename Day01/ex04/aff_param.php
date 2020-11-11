a55aa921c0b8185fe973995011d5a123b35335e9
root <yel-kobi@e2r10p15.1337.ma>
2020-11-11 22:26:55 +0100

    ss

--- Day01/ex04/aff_param.php
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/php
-<?php
-$x=1;
-while(--$argc)
-    printf($argv[$x++]."\n");
-?>
--- Day01/ex08/ft_is_sort.php
+++
@@ -1,9 +0,0 @@
-<?php
-function ft_is_sort($str)
-{
-    $string = $str;
-    if (sort($string) ===  $str) 
-        return (1);
-    return (0);
-}
-?>
